66a73e2c974eadb4cb9f2cfbe772f67b9bbe9ff4
cucu8 <cuneytdonmez8@gmail.com>
2026-02-25 10:27:48 +0300

    fix vite config

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/dashboard/">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 )
